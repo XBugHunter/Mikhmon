b48f68b998fddf53d824583834d129b820781b2e
XBugHunter <temp.portfolio.dashboard@gmail.com>
2023-08-27 18:51:26 +0700

    push

---
+++ mikhmon/config/config.php
@@ -0,0 +1,3 @@
+<?php if(substr($_SERVER["REQUEST_URI"], -10) == "config.php"){header("Location:./");}; 
+$data['mikhmon'] = array ('1'=>'mikhmon<|<mikhmon','mikhmon>|>aWNlbA==');
+$data['rudi'] = array ('1'=>'rudi!192.168.100.1','rudi@|@admin','rudi#|#','rudi%','rudi^','rudi&Rp','rudi*','rudi(','rudi)','rudi=30','rudi@!@disable','rudi#!#');
